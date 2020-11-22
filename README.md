cbf0f0773997fcc3b727f6b8939448c4b88ed8c7
Grandclosing <vukpetrovic1@yahoo.com>
2020-11-22 16:30:55 -0500

    Updated readme; ran prettier

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
 
-## Hello world
+## Blend Mode Demo
 
-`// todo `
+I still find myself surprised by CSS properties I've never seen or heard of before. One such property is `mix-blend-mode`. MDN has some nice [documentation](https://developer.mozilla.org/en-US/docs/Web/CSS/mix-blend-mode) on it.
+
+As soon as I discovered this property, I wanted to build something that showcased all possible values for it. You can [play with the demo](http://blendmode.surge.sh/).
+
+### CORS
+Most image sharing sites don't enable [cross-origin resource sharing](https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS), so try to use [Imgur](https://imgur.com/) or another site you know of with lax CORS policies. 
+
+![Screenshot](/screenshots/screenshot.png)
+
+MIT License
+
+Copyright (c) 2020 Vuk Petrovic
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- public/js/main.js
+++ public/js/main.js
@@ -1,121 +1,124 @@
-
 const blendModes = [
-    'normal',
-    'multiply',
-    'screen',
-    'overlay',
-    'darken',
-    'lighten',
-    'color-dodge',
-    'color-burn',
-    'difference',
-    'exclusion',
-    'hue',
-    'saturation',
-    'color',
-    'luminosity'
-]
+  "normal",
+  "multiply",
+  "screen",
+  "overlay",
+  "darken",
+  "lighten",
+  "color-dodge",
+  "color-burn",
+  "difference",
+  "exclusion",
+  "hue",
+  "saturation",
+  "color",
+  "luminosity",
+];
 
 let imageLoaded = false;
 let currentURL = "";
 
 function isValidURL(str) {
-    try {
-        url = new URL(str);
+  try {
+    url = new URL(str);
 
-        return true;
-    } catch (_) {
-        return false;  
-    }
+    return true;
+  } catch (_) {
+    return false;
+  }
 }
 
 function processKey(e) {
-    const key = e.which || e.keyCode;
+  const key = e.which || e.keyCode;
 
-    if(key == 13 /* enter */)
-        search();
+  if (key == 13 /* enter */) search();
 }
 
 function shakeSearchbar() {
-    const urlEntry = document.getElementsByClassName('url-entry')[0];
-    urlEntry.classList.toggle('bad-url');
+  const urlEntry = document.getElementsByClassName("url-entry")[0];
+  urlEntry.classList.toggle("bad-url");
 
-    setTimeout(function() {
-        urlEntry.classList.toggle('bad-url');
-    }, 1000);
+  setTimeout(function () {
+    urlEntry.classList.toggle("bad-url");
+  }, 1000);
 }
 
 function search() {
-    const urlEntry = document.getElementsByClassName('url-entry')[0];
-    const url = urlEntry.value;
+  const urlEntry = document.getElementsByClassName("url-entry")[0];
+  const url = urlEntry.value;
 
-    if(url == currentURL)
-        return;
+  if (url == currentURL) return;
 
-    currentURL = url;
+  currentURL = url;
 
-    if(!isValidURL(url)) {
-        shakeSearchbar();
+  if (!isValidURL(url)) {
+    shakeSearchbar();
 
-        return;
-    }
+    return;
+  }
 
-    loadImage(url);
+  loadImage(url);
 }
 
 function loadImage(url) {
-    let boxes = document.getElementsByClassName('grid-element');
-    let saveButtons = document.getElementsByClassName('save-image-button');
+  let boxes = document.getElementsByClassName("grid-element");
+  let saveButtons = document.getElementsByClassName("save-image-button");
 
-    let rootStyle = document.documentElement.style;
-    rootStyle.setProperty('--empty-box-padding', '0px');
+  let rootStyle = document.documentElement.style;
+  rootStyle.setProperty("--empty-box-padding", "0px");
 
-    document.getElementsByClassName('no-pic-container')[0].style.display = 'none';
-    document.body.style.overflowY = 'scroll';
+  document.getElementsByClassName("no-pic-container")[0].style.display = "none";
+  document.body.style.overflowY = "scroll";
 
-    for(let i = 0; i < boxes.length; ++i) {
-        let box = boxes[i];
-        let imageNode = document.createElement('img');
-        imageNode.setAttribute('src', url);
-        imageNode.style.mixBlendMode = blendModes[i];
+  for (let i = 0; i < boxes.length; ++i) {
+    let box = boxes[i];
+    let imageNode = document.createElement("img");
+    imageNode.setAttribute("src", url);
+    imageNode.style.mixBlendMode = blendModes[i];
 
-        if(box.firstChild)
-            box.removeChild(box.firstChild);
+    if (box.firstChild) box.removeChild(box.firstChild);
 
-        box.appendChild(imageNode);
+    box.appendChild(imageNode);
 
-        saveButtons[i].classList.remove('save-disabled');
-    }
+    saveButtons[i].classList.remove("save-disabled");
+  }
 
-    imageLoaded = true;
+  imageLoaded = true;
 }
 
 function updateColor() {
-    document.documentElement.style.setProperty('--blend-color', 
-        document.getElementsByClassName('color-picker')[0].value);
+  document.documentElement.style.setProperty(
+    "--blend-color",
+    document.getElementsByClassName("color-picker")[0].value
+  );
 }
 
 function save(id) {
-    if(!imageLoaded) return;
+  if (!imageLoaded) return;
 
-    let box = document.getElementsByClassName('grid-element')[id];
-    let fileName = blendModes[id] + ".png";
+  let box = document.getElementsByClassName("grid-element")[id];
+  let fileName = blendModes[id] + ".png";
 
-    domtoimage.toBlob(box)
-        .then(function(blob) {
-            window.saveAs(blob, fileName);
-        })
-        .catch(function(e) {
-            shakeSearchbar();
+  domtoimage
+    .toBlob(box)
+    .then(function (blob) {
+      window.saveAs(blob, fileName);
+    })
+    .catch(function (e) {
+      shakeSearchbar();
 
-            openModal();
-        });
+      openModal();
+    });
 }
 
 function openModal() {
-    document.getElementsByClassName('modal-background')[0].classList.add('modal-visible');
+  document
+    .getElementsByClassName("modal-background")[0]
+    .classList.add("modal-visible");
 }
 
 function closeModal() {
-    document.getElementsByClassName('modal-background')[0].classList.remove('modal-visible');
+  document
+    .getElementsByClassName("modal-background")[0]
+    .classList.remove("modal-visible");
 }
